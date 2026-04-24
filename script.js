d6f1c5c478148e0955fadf3b7c9153fcfbba3327
estwilliamfel-dev <est.williamf.torres@unimilitar.edu.co>
2026-04-24 09:35:01 -0500

    -Base proporcionada por el profesor añadida

---
+++ script.js
@@ -0,0 +1,33 @@
+/**
+ * Universidad - Facultad de Ingeniería
+ * Asignatura: Introducción a la Computación Gráfica
+ * * Estudiante: William Felipe Torres Varela
+ * * Tarea: Implementar los algoritmos de rasterización manual.
+ */
+
+// Función de apoyo para dibujar un píxel individual
+function drawPixel(ctx, x, y, color = "#000000") {
+    ctx.fillStyle = color;
+    ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
+
+/**
+ * Implementación del algoritmo de Bresenham para líneas.
+ * @param {number} x0, y0 - Coordenadas iniciales
+ * @param {number} x1, y1 - Coordenadas finales
+ * @returns {void}
+ */
+function bresenhamLine(x0, y0, x1, y1, color) {
+    // Desarrollo del estudiante
+}
+
+/**
+ * Calcula los vértices de un polígono regular.
+ * @param {number} centerX, centerY - Centro
+ * @param {number} sides - Número de lados
+ * @param {number} radius - Radio
+ * @returns {Array} Arreglo de objetos {x, y}
+ */
+function getPolygonVertices(centerX, centerY, sides, radius) {
+    // Desarrollo del estudiante (Uso de Math.sin/Math.cos y retorno de datos)
+}
